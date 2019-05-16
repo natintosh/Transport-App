62975d3983990088ce5a13a369a9dbc009110749
Oluwatobiloba Ogunye <ogunye4nao@gmail.com>
2019-05-16 22:52:53 +0100

    Hotfix

---
+++ lib/pages/bus_details.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class BusDetailsPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Bus Details"),
+      ),
+    );
+  }
+}
